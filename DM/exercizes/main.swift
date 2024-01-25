87f48a4b5c86f2713d0324bbde5e0e320fdaff44
DanielComputerScience <elijah.williams@student.allenisd.org>
2024-01-25 10:20:44 -0600

    adding repository files

---
+++ DM/exercizes/main.swift
@@ -0,0 +1,25 @@
+func cleardomain(domain: inout [Int]) {
+    for elements in 0 ..< domain.count {
+        for checks in elements + 1 ..< domain.count {
+            
+          if domain[element] = domain] {
+            
+func isInjective(domain: [Int], range: [Int]) -> Bool {
+    if domain.count <= range.count {
+        return true
+    }
+}
+      
+func isBijective(range: [Int], domain: [Int]) -> Bool {
+    if domain = range {
+        return true
+    }
+}
+func isSurjective(range: [Int], domain: [Int]) -> Bool {
+    if range <= domain {
+        return true
+    }
+}
+
+
+
